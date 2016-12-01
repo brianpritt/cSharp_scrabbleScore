2c880456daa3570c5f01c318c92c1e59b8ec22fb
Brian Pritt <brianpritt@gmail.com>
2016-12-01 15:18:09 -0800

    add nancy framework to site, with basic functionallity

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,18 +1,23 @@
-// using Nancy;
-// using System.Collections.Generic;
-// using System;
-// using TEMPLATE.Objects;
-//
-// namespace TEMPLATE
-// {
-//   public class HomeModule : NancyModule
-//   {
-//     public HomeModule()
-//     {
-//       Get["/"] = _ => {
-//         return View["TEMPLATE.cshtml"];
-//       };
-//
-//     }
-//   }
-// }
+using Nancy;
+using System.Collections.Generic;
+using System;
+using ScrabbleScore.Objects;
+
+namespace ScrabbleScore
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        return View["index.cshtml"];
+      };
+      Post["/result"] = _ => {
+        Score newScore = new Score(Request.Form["word"]);
+        int finalScore = newScore.ScoreCalc();
+        return View["results.cshtml", newScore];
+      };
+
+    }
+  }
+}
